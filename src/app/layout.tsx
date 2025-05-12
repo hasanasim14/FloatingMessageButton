4dd6c6f080c287776c78fe49fbbe4abe68606bd2
Hasan Aasim <hasan.asim14@gmail.com>
2025-05-12 16:02:30 +0500

    Changed the background color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-950`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-900`}
       >
         {/* <Navbar /> */}
         {children}
